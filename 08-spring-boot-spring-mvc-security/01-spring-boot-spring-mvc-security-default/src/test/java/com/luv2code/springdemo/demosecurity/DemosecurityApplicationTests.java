b9d8fe985796f2c81fec713d9cf9e03ce34df383
Jami Huang <jamhuang2@publicisgroupe.net>
2024-03-20 11:08:31 -0500

    finished section 7 spring MVC crud

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/test/java/com/luv2code/springdemo/demosecurity/DemosecurityApplicationTests.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/test/java/com/luv2code/springdemo/demosecurity/DemosecurityApplicationTests.java
@@ -1,10 +1,10 @@
-package com.luv2code.springboot.cruddemo;
+package com.luv2code.springdemo.demosecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CruddemoApplicationTests {
+class DemosecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
